74be6169ff2f868278849aadef8122c8f4256a3a
Forl <kumaloveslife@gmail.com>
2019-05-07 00:35:23 +0800

    tree shaking 去除未引用的代码

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
-import _ from 'lodash'
+//import _ from 'lodash'
 import printMe from './print'
 import './style.css'
+import {cube} from './main'
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button')
-  element.innerHTML = _.join(['Hello', 'webpack change'], ' ');
+  var element = document.createElement('pre');
 
-  btn.innerHTML = 'Click me and check the console'
-  btn.onclick = printMe
-
-  element.appendChild(btn)
+  element.innerHTML = [
+    'hello webpack',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n')
 
   return element;
 }
 
 let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
 document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function () {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component(); // 重新渲染页面后，component 更新 click 事件处理
-    document.body.appendChild(element);
-  })
-}
