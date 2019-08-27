a2492d7615f40a33dd2cc5cd52026eaa8756f4ec
Forl <kumaloveslife@gmail.com>
2019-08-27 20:33:21 +0800

    配置分析工具，可参考guide>code split>bundle analysis

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
-function getComponent(){
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-    let el = document.createElement('p')
+//import _ from 'lodash'  同步载入
+
+// 懒加载
+async function getComponent(){
+    const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash')
+    const el = document.createElement('p')
     el.innerText = _.join(['Dell','Lee'],'-')
     return el
-  })
 }
+document.addEventListener('click',()=>{
+  getComponent().then(e => {
+    document.body.appendChild(e)
+  })
+})
 
+// 如果不配置cacheGroups，则打包成 0.js
 // function getComponent(){
 //   return import('lodash').then(({ default: _ }) => {
 //     let el = document.createElement('p')
@@ -14,11 +22,6 @@ function getComponent(){
 //   })
 // }
 
-getComponent().then(e => {
-  document.body.appendChild(e)
-})
-// document.addEventListener('click',()=>{
-//   getComponent().then(e => {
-//     document.body.appendChild(e)
-//   })
+// getComponent().then(e => {
+//   document.body.appendChild(e)
 // })
